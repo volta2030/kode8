b004ecf14ef9b32db4db4ed6fa427607a1a4d418
volta2030 <volta2030@gmail.com>
2023-09-30 23:20:00 +0900

    -Add Table()

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,10 +1,9 @@
 import Utility.Companion.toHex
 import Utility.Companion.trim
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.DisplayStyle.Companion.TableRow
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 import org.khronos.webgl.ArrayBuffer
@@ -16,9 +15,15 @@ import org.w3c.files.get
 
 
 fun main() {
+    val cols = 64
+    val chunkSize = 2
+
     var selectedFile: File? = null
     var string by mutableStateOf("")
-    var byteData by mutableStateOf<ByteArray?>(null)
+    var rows by mutableStateOf(0)
+    var cellData by mutableStateOf(
+        MutableList(1) { List(cols) { "" } }
+    )
 
 
     renderComposable(rootElementId = "root") {
@@ -40,7 +45,8 @@ fun main() {
 
         Div({ style {
             position(Position.Absolute)
-            top(40.px)
+            top(30.px)
+            padding(20.px)
         } }) {
             val fileInput = Input(
                 type = InputType.File,
@@ -54,9 +60,20 @@ fun main() {
                             val arrayBuffer = event.target.asDynamic().result as? ArrayBuffer
                             if (arrayBuffer != null) {
                                 val byteArray = Int8Array(arrayBuffer).unsafeCast<ByteArray>()
-                                byteData = byteArray
-                                string = byteArray.toHex().trim(16)
+                                cellData.clear()
+                                string = byteArray.toHex()
+                                rows = (byteArray.size -1) / cols + 1
 
+                                for (i in 0 until rows) {
+                                    val rowList = mutableListOf<String>()
+                                    for (j in 0 until cols) {
+                                        val startIndex = i * cols * chunkSize + j * chunkSize
+                                        val endIndex = startIndex + chunkSize
+                                        val chunk = string.substring(startIndex, endIndex)
+                                        rowList.add(chunk)
+                                    }
+                                    cellData.add(rowList)
+                                }
                             }
                         }
                         selectedFile?.let { fileReader.readAsArrayBuffer(it) }
@@ -64,12 +81,23 @@ fun main() {
                 }
             )
 
-            Text(string)
+            Div({
+                style {
+                    padding(5.px)
+                    fontSize(15.px)
+                }
+            }
+            ){
+                Table{
+                    TableHeader(cols)
+                    TableRows(cols, rows, cellData)
+                }
+            }
         }
 
         Footer({
             style {
-                position(Position.Absolute)
+                position(Position.Fixed)
                 bottom(0.px)
                 left(0.px)
                 right(0.px)
@@ -81,5 +109,28 @@ fun main() {
             Text("Copyright © 2023 SnackLab. All Rights Reserved.")
         }
 
+    }
+}
+
+@Composable
+fun TableHeader(cols : Int){
+
+    repeat(cols){ i->
+        Th{
+            Text(i.toHex())
+        }
+    }
+}
+
+@Composable
+fun TableRows(cols : Int, numberOfRows : Int, cellData : List<List<String>>){
+    repeat(numberOfRows) { i ->
+        Tr{
+            repeat(cols){j ->
+                Td {
+                    Text(cellData[i][j])
+                }
+            }
+        }
     }
 }
