3f6cfa375b9a39afda1cdac9ed92c497e3999698
volta2030 <volta2030@gmail.com>
2023-09-30 23:16:03 +0900

    -Add new function Int.toHex()

--- src/jsMain/kotlin/Utility.kt
+++ src/jsMain/kotlin/Utility.kt
@@ -1,5 +1,10 @@
 class Utility {
     companion object {
+        fun Int.toHex() : String{
+            // 정수를 두 자리 16진수 문자열로 변환
+            return this.toString(16).padStart(2, '0').uppercase()
+        }
+
         fun ByteArray.toHex(): String {
             val hexChars = "0123456789ABCDEF"
             val result = StringBuilder(this.size * 2)
@@ -20,10 +25,10 @@ class Utility {
             var trimedString = ""
 
             for (i : Int in indices step column){
-                trimedString = this.slice(i until i + column) + "/n"
+                trimedString += this.slice(i until i + column) + "\n"
             }
 
-            return ""
+            return trimedString
         }
     }
 }
