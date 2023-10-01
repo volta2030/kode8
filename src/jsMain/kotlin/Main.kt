615c9d7fbe45363e8fdfddd8f294b8d62b6691c6
volta2030 <volta2030@gmail.com>
2023-10-01 22:19:46 +0900

    -Redesign...

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import Utility.Companion.toHex
 import androidx.compose.runtime.*
 import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.size
 import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.keywords.CSSAutoKeyword
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 import org.khronos.webgl.ArrayBuffer
@@ -12,98 +14,115 @@ import org.w3c.files.FileReader
 import org.w3c.files.get
 
 fun main() {
+
     val cols = 64
     val chunkSize = 2
-
     var selectedFile: File? = null
     var string by mutableStateOf("")
     var rows by mutableStateOf(0)
+    var size by mutableStateOf(0)
     var cellData by mutableStateOf(
         Array(1) { Array(cols) { "" } }
     )
 
 
     renderComposable(rootElementId = "root") {
-
-        Header({
+        Div({
             style {
-                position(Position.Absolute)
-                top(0.px)
-                left(0.px)
-                right(0.px)
-                color(Color.white)
-                fontWeight(2)
-                backgroundColor(Color.rebeccapurple)
-                padding(10.px)
-                textAlign("center")
-            }}){
-                Text("kode8 - The Byte Code Viewer")
+                display(DisplayStyle.Flex)
+                flexDirection(FlexDirection.Column)
+                justifyContent(JustifyContent.SpaceBetween)
             }
+        }) {
+            Header({
+                style {
+                    position(Position.Fixed)
+                    top(0.px)
+                    left(0.px)
+                    right(0.px)
+                    color(Color.white)
+                    fontWeight(3)
+                    backgroundColor(Color.rebeccapurple)
+                    display(DisplayStyle.Flex)
+                    justifyContent(JustifyContent.SpaceBetween)
+                    textAlign("center")
+                    padding(5.px)
+                }}){
 
-        Div({ style {
-            position(Position.Absolute)
-            top(30.px)
-            padding(20.px)
-        } }) {
-            val fileInput = Input(
-                type = InputType.File,
-                attrs = {
-                    onChange { e ->
-                        val target = e.target as? HTMLInputElement
-                        selectedFile = target?.files?.get(0)
+                Text("kode8 - The Byte Code Viewer")
 
-                        val fileReader = FileReader()
-                        fileReader.onload = { event ->
-                            val arrayBuffer = event.target.asDynamic().result as? ArrayBuffer
-                            if (arrayBuffer != null) {
-                                val byteArray = Int8Array(arrayBuffer).unsafeCast<ByteArray>()
-                                string = byteArray.toHex()
-                                rows = (byteArray.size -1) / cols + 1
+                Input(
+                    type = InputType.File,
+                    attrs = {
+                        onChange { e ->
+                            val target = e.target as? HTMLInputElement
+                            selectedFile = target?.files?.get(0)
 
-                                cellData = Array(rows) {Array(cols) {""}}
-                                for (i in 0 until rows) {
-                                    for (j in 0 until cols) {
-                                        val startIndex = i * cols * chunkSize + j * chunkSize
-                                        val endIndex = startIndex + chunkSize
-                                        cellData[i][j] = string.substring(startIndex, endIndex)
+                            val fileReader = FileReader()
+                            fileReader.onload = { event ->
+                                val arrayBuffer = event.target.asDynamic().result as? ArrayBuffer
+                                if (arrayBuffer != null) {
+                                    val byteArray = Int8Array(arrayBuffer).unsafeCast<ByteArray>()
+                                    size = byteArray.size
+                                    string = byteArray.toHex()
+                                    rows = (byteArray.size -1) / cols + 1
+                                    cellData = Array(rows) {Array(cols) {""}}
+                                    for (i in 0 until rows) {
+                                        for (j in 0 until cols) {
+                                            val startIndex = i * cols * chunkSize + j * chunkSize
+                                            val endIndex = startIndex + chunkSize
+                                            cellData[i][j] = string.substring(startIndex, endIndex)
+                                        }
                                     }
                                 }
                             }
+                            selectedFile?.let { fileReader.readAsArrayBuffer(it) }
                         }
-                        selectedFile?.let { fileReader.readAsArrayBuffer(it) }
                     }
-                }
-            )
+                )
 
-            Div({
-                style {
-                    padding(5.px)
-                    fontSize(15.px)
-                }
             }
-            ){
-                Table{
+
+            Main({ style {
+                paddingLeft(5.px)
+                paddingTop(40.px)
+                paddingBottom(40.px)
+                paddingRight(5.px)
+
+                display(DisplayStyle.Flex)
+                flexDirection(FlexDirection.Column)
+                justifyContent(JustifyContent.SpaceBetween)
+
+            } }) {
+                Table({
+                    style {
+                        fontSize(15.px)
+                        border (1.px, LineStyle.Solid, Color.black)
+                    }
+                }){
                     TableHeader(cols)
                     TableRows(cols, rows, cellData)
                 }
+
+                Text("Copyright © 2023 SnackLab(volta2030). All Rights Reserved.")
+            }
+
+            Footer({
+                style {
+                    position(Position.Fixed)
+                    bottom(0.px)
+                    left(0.px)
+                    right(0.px)
+                    backgroundColor(Color.rebeccapurple) // Add background color as needed
+                    textAlign("right")
+                    padding(5.px)
+                    color(Color.white)
+                    fontWeight(3)
+                }
+            }) {
+                Text("$size Bytes")
             }
         }
-
-        Footer({
-            style {
-                position(Position.Fixed)
-                bottom(0.px)
-                left(0.px)
-                right(0.px)
-                backgroundColor(Color.lightgray) // Add background color as needed
-                padding(10.px)
-                textAlign("center")
-            }
-        }) {
-
-            Text("Copyright © 2023 SnackLab(volta2030). All Rights Reserved.")
-        }
-
     }
 }
 
