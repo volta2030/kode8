b2c3c01c97c5323258f774fa1a6b09052e9ee4fb
volta2030 <volta2030@gmail.com>
2023-10-03 22:40:48 +0900

    -Modify DisplayMode

--- src/jsMain/kotlin/DisplayMode.kt
+++ src/jsMain/kotlin/DisplayMode.kt
@@ -1,6 +1,6 @@
-enum class DisplayMode(val isSelected : Boolean, val chunkSize : Int) {
-    BINARY(false, 8),
-    INT(false, 3),
-    HEX(false, 2),
-    ASCII(false, 1)
+enum class DisplayMode(val chunkSize : Int) {
+    BINARY(8),
+    INT(3),
+    HEX(2),
+    ASCII(1)
 }
