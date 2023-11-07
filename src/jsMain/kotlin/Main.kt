1cf7d5c3d89fe394910f3a3b99ce8ee19e59f889
volta2030 <volta2030@gmail.com>
2023-11-07 22:34:28 +0900

    -Modify keydown event : #50

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -41,7 +41,7 @@ import util.DataProcessor.Companion.trimmedCellData
 import util.DataProcessor.Companion.updateCellData
 import util.DataProcessor.Companion.updateTrimmedCellData
 
-const val version = "1.16.0"
+const val version = "1.16.1"
 
 fun main() {
 
